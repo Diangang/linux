97dd83ab6aed72473e7ab9bba847fb0abc6a7b5a
Diangang Li <lidiangang@bytedance.com>
2026-05-12 16:53:06 +0800

    refactor(裁剪): 删除一批头文件配置残留
    - 将 DMAR_DEBUG、DMA_OF 和 CXL_SUSPEND 的残留头文件分支收敛为存根
    - 保留删除后树内实际生效的一侧，移除已无 Kconfig 定义的条件编译

--- include/linux/of_dma.h
+++ include/linux/of_dma.h
@@ -31,47 +31,6 @@ struct of_dma_filter_info {
 	dma_filter_fn	filter_fn;
 };
 
-#ifdef CONFIG_DMA_OF
-extern int of_dma_controller_register(struct device_node *np,
-		struct dma_chan *(*of_dma_xlate)
-		(struct of_phandle_args *, struct of_dma *),
-		void *data);
-extern void of_dma_controller_free(struct device_node *np);
-
-static void __of_dma_controller_free(void *np)
-{
-	of_dma_controller_free(np);
-}
-
-static inline int
-devm_of_dma_controller_register(struct device *dev, struct device_node *np,
-				struct dma_chan *(*of_dma_xlate)
-				(struct of_phandle_args *, struct of_dma *),
-				void *data)
-{
-	int ret;
-
-	ret = of_dma_controller_register(np, of_dma_xlate, data);
-	if (ret)
-		return ret;
-
-	return devm_add_action_or_reset(dev, __of_dma_controller_free, np);
-}
-
-extern int of_dma_router_register(struct device_node *np,
-		void *(*of_dma_route_allocate)
-		(struct of_phandle_args *, struct of_dma *),
-		struct dma_router *dma_router);
-#define of_dma_router_free of_dma_controller_free
-
-extern struct dma_chan *of_dma_request_slave_channel(struct device_node *np,
-						     const char *name);
-extern struct dma_chan *of_dma_simple_xlate(struct of_phandle_args *dma_spec,
-		struct of_dma *ofdma);
-extern struct dma_chan *of_dma_xlate_by_chan_id(struct of_phandle_args *dma_spec,
-		struct of_dma *ofdma);
-
-#else
 static inline int of_dma_controller_register(struct device_node *np,
 		struct dma_chan *(*of_dma_xlate)
 		(struct of_phandle_args *, struct of_dma *),
@@ -116,7 +75,5 @@ static inline struct dma_chan *of_dma_simple_xlate(struct of_phandle_args *dma_s
 }
 
 #define of_dma_xlate_by_chan_id NULL
-
-#endif
 
 #endif /* __LINUX_OF_DMA_H */
