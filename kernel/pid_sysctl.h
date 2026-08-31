29281c93dbc7d1eb61d2d43bca5fd98faa32b008
Diangang Li <lidiangang@bytedance.com>
2026-08-31 18:59:39 +0800

    refactor(trim): remove orphaned internal headers

--- kernel/pid_sysctl.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef LINUX_PID_SYSCTL_H
-#define LINUX_PID_SYSCTL_H
-
-#include <linux/pid_namespace.h>
-
-static inline void register_pid_ns_sysctl_table_vm(void) {}
-
-#endif /* LINUX_PID_SYSCTL_H */
