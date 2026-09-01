0e5be7f3b57c733ed0dceb29af387139db66ff25
Diangang Li <lidiangang@bytedance.com>
2026-09-01 15:47:33 +0800

    refactor(trim): remove orphaned ACPI NUMA stubs

--- include/acpi/acpi_numa.h
+++ include/acpi/acpi_numa.h
@@ -2,20 +2,8 @@
 #ifndef __ACPI_NUMA_H
 #define __ACPI_NUMA_H
 
-static inline int fix_pxm_node_maps(int max_nid)
-{
-	return 0;
-}
 static inline void disable_srat(void)
 {
-}
-static inline int pxm_to_node(int pxm)
-{
-	return 0;
-}
-static inline int node_to_pxm(int node)
-{
-	return 0;
 }
 
 static inline void disable_hmat(void)
