93df8fb724f42e043a4d958a66bb942df5ce94ab
Diangang Li <lidiangang@bytedance.com>
2026-09-01 15:02:12 +0800

    refactor(trim): remove inert PCI runtime PM enable

--- drivers/pci/bus.c
+++ drivers/pci/bus.c
@@ -15,7 +15,6 @@
 #include <linux/of.h>
 #include <linux/of_platform.h>
 #include <linux/platform_device.h>
-#include <linux/pm_runtime.h>
 #include <linux/proc_fs.h>
 #include <linux/slab.h>
 
@@ -360,13 +359,6 @@ void pci_bus_add_device(struct pci_dev *dev)
 
 	/* Save config space for error recoverability */
 	pci_save_state(dev);
-
-	/*
-	 * Enable runtime PM, which potentially allows the device to
-	 * suspend immediately, only after the PCI state has been
-	 * configured completely.
-	 */
-	pm_runtime_enable(&dev->dev);
 
 	if (!dn || of_device_is_available(dn))
 		pci_dev_allow_binding(dev);
