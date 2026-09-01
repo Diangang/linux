5612ca17246c364fe1e411e6b80e491a5b309ecd
Diangang Li <lidiangang@bytedance.com>
2026-09-01 15:45:23 +0800

    refactor(trim): remove unused wakeup source state

--- include/linux/pm_wakeup.h
+++ include/linux/pm_wakeup.h
@@ -13,56 +13,6 @@
 # error "Please do not include this file directly."
 #endif
 
-#include <linux/types.h>
-
-struct wake_irq;
-
-/**
- * struct wakeup_source - Representation of wakeup sources
- *
- * @name: Name of the wakeup source
- * @id: Wakeup source id
- * @entry: Wakeup source list entry
- * @lock: Wakeup source lock
- * @wakeirq: Optional device specific wakeirq
- * @timer: Wakeup timer list
- * @timer_expires: Wakeup timer expiration
- * @total_time: Total time this wakeup source has been active.
- * @max_time: Maximum time this wakeup source has been continuously active.
- * @last_time: Monotonic clock when the wakeup source's was touched last time.
- * @prevent_sleep_time: Total time this source has been preventing autosleep.
- * @event_count: Number of signaled wakeup events.
- * @active_count: Number of times the wakeup source was activated.
- * @relax_count: Number of times the wakeup source was deactivated.
- * @expire_count: Number of times the wakeup source's timeout has expired.
- * @wakeup_count: Number of times the wakeup source might abort suspend.
- * @dev: Struct device for sysfs statistics about the wakeup source.
- * @active: Status of the wakeup source.
- * @autosleep_enabled: Autosleep is active, so update @prevent_sleep_time.
- */
-struct wakeup_source {
-	const char 		*name;
-	int			id;
-	struct list_head	entry;
-	spinlock_t		lock;
-	struct wake_irq		*wakeirq;
-	struct timer_list	timer;
-	unsigned long		timer_expires;
-	ktime_t total_time;
-	ktime_t max_time;
-	ktime_t last_time;
-	ktime_t start_prevent_time;
-	ktime_t prevent_sleep_time;
-	unsigned long		event_count;
-	unsigned long		active_count;
-	unsigned long		relax_count;
-	unsigned long		expire_count;
-	unsigned long		wakeup_count;
-	struct device		*dev;
-	bool			active:1;
-	bool			autosleep_enabled:1;
-};
-
 static inline void device_set_wakeup_capable(struct device *dev, bool capable)
 {
 	dev->power.can_wakeup = capable;
