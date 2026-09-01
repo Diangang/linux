79eba940b04371695d4b604502ee97be9e3ffed5
Diangang Li <lidiangang@bytedance.com>
2026-09-01 12:29:18 +0800

    refactor(trim): remove unused PSCI state helper

--- drivers/firmware/psci/psci.c
+++ drivers/firmware/psci/psci.c
@@ -89,15 +89,6 @@ bool psci_has_osi_support(void)
 	return psci_cpu_suspend_feature & PSCI_1_0_OS_INITIATED;
 }
 
-static inline bool psci_power_state_loses_context(u32 state)
-{
-	const u32 mask = psci_has_ext_power_state() ?
-					PSCI_1_0_EXT_POWER_STATE_TYPE_MASK :
-					PSCI_0_2_POWER_STATE_TYPE_MASK;
-
-	return state & mask;
-}
-
 bool psci_power_state_is_valid(u32 state)
 {
 	const u32 valid_mask = psci_has_ext_power_state() ?
